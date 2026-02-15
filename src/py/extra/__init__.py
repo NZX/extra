a2f7e0c2a8cb1cdcde30234c7b648930cf62d6a8
Michael Green <michael.green@nzx.comn>
2026-02-16 12:05:50 +1300

    [Cleanup] Reformat import statements in __init__.py for improved readability

--- src/py/extra/__init__.py
+++ src/py/extra/__init__.py
@@ -1,9 +1,9 @@
-from .http.model import (
-    HTTPRequest,  # NOQA: F401
-    HTTPResponse,  # NOQA: F401
-    HTTPResponseLine,  # NOQA: F401
-    HTTPRequestError,  # NOQA: F401
-)  # NOQA: F401
+from .http.model import (  # NOQA: F401
+    HTTPRequest,
+    HTTPResponse,
+    HTTPResponseLine,
+    HTTPRequestError,
+)
 from .decorators import on, expose, pre, post  # NOQA: F401
 from .server import run  # NOQA: F401
 from .model import Service  # NOQA: F401
